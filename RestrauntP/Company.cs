76b9d197fe427aefa6e46ae034604b7df6d01d7a
Zak <zak.henderson.47@gmail.com>
2023-10-03 08:20:13 -0500

    Add project files.

---
+++ RestrauntP/Company.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestrauntP
+{
+    public class Company
+    {
+        public int RANK { get; set; }
+        public string LOGO { get; set; }
+        public string COMPANY { get; set; }
+        public string CATEGORY { get; set; }
+
+        [JsonProperty("2018 US SYSTEMWIDE SALES MILLIONS")]
+        public double SALES_MILLIONS_2018_US { get; set; }
+
+        public Company()
+        {
+            RANK = 0;
+            LOGO = string.Empty;
+            COMPANY = string.Empty;
+            CATEGORY = string.Empty;
+            SALES_MILLIONS_2018_US = 0;
+        }
+
+        public override string ToString()
+        {
+            return $"{COMPANY}";
+        }
+    }
+        
+    
+}
---
+++ RestrauntP/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.Json;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace RestrauntP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private List<Company> restaurantDataList;
+        public double MinSales { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string JsonPath = File.ReadAllText("RestrauntInformationFile.json");
+
+            List<Company> CompanyData = JsonConvert.DeserializeObject<List<Company>>(JsonPath);
+
+            foreach (Company company in CompanyData)
+            {
+                lstCompany.Items.Add(company);
+            }
+            
+
+
+    }
+
+        private void lstCompany_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string JsonPath = File.ReadAllText("RestrauntInformationFile.json");
+
+            List<Company> CompanyData = JsonConvert.DeserializeObject<List<Company>>(JsonPath);
+
+            int selectIndex = lstCompany.SelectedIndex;
+
+            if(selectIndex >= 0 && selectIndex < CompanyData.Count)
+            {
+                Company SelectedCompany = CompanyData[selectIndex];
+                string imageUrl = CompanyData[selectIndex].LOGO;
+                string rank = (selectIndex + 1).ToString();
+                double sales = SelectedCompany.SALES_MILLIONS_2018_US;
+                string category = SelectedCompany.CATEGORY;
+
+                if (!string.IsNullOrWhiteSpace(imageUrl))
+                {
+                    try
+                    {
+                        BitmapImage image = new BitmapImage(new Uri(imageUrl));
+                        imgLogo.Source = image;
+                    }
+                    catch {
+                    
+                    imgLogo.Source = null;
+                    }
+                }
+                lblRank.Content = $"Rank: {rank}";
+                lblSales.Content = $"Sales (Millions): {sales}";
+                lblCategory.Content = $"Category: {category}";
+            }
+
+
+
+        }
+
+        private void btnMin_Click(object sender, RoutedEventArgs e)
+        {
+            string JsonPath = File.ReadAllText("RestrauntInformationFile.json");
+
+            List<Company> CompanyData = JsonConvert.DeserializeObject<List<Company>>(JsonPath);
+
+            if (double.TryParse(txtBox.Text, out double minSales))
+            {
+                List<string> filteredCompanies = restaurantDataList
+                    .Where(Company => Company.SALES_MILLIONS_2018_US >= minSales)
+                    .Select(Company => Company.COMPANY)
+                    .ToList();
+
+                lstCompany.ItemsSource = filteredCompanies;
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid minimum sales value.", "Invalid Input", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+    }
+}
